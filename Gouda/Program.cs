89dc5f3a6986066663a1c4685eeaa1458831ae53
Ben Moriarty <benmoriarty2004@gmail.com>
2026-05-25 08:03:06 -0700

    gitignore

--- Gouda/Program.cs
+++ Gouda/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
